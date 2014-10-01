35c42649740abb5ee7d59d073cfc2fdb14049b69
Myles Borins <myles@famo.us>
2014-10-01 13:35:00 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#famous global seed
+> A Seed Project to get started with the global build of Famo.us
+
+##Getting Started
+
+To get started with this project you can choose to either git-clone or [download the zip](https://github.com/Famous/famous-global-seed/archive/master.zip)
+
+open index.html, and start writing code in src/app.js
+
+That's it!!!
+
+## Contributing
+All contributions are welcome! The simplest way to show your support for this project is to **"star" it**.
+
+##License
+ISC
---
+++ src/main.js
@@ -0,0 +1,27 @@
+/*global famous*/
+// import dependencies
+var Engine = famous.core.Engine;
+var Modifier = famous.core.Modifier;
+var Transform = famous.core.Transform;
+var ImageSurface = famous.surfaces.ImageSurface;
+
+// create the main context
+var mainContext = Engine.createContext();
+
+// your app here
+var logo = new ImageSurface({
+    size: [200, 200],
+    content: 'http://code.famo.us/assets/famous_logo.svg',
+    classes: ['double-sided']
+});
+
+var initialTime = Date.now();
+var centerSpinModifier = new Modifier({
+    origin: [0.5, 0.5],
+    align: [0.5, 0.5],
+    transform : function () {
+        return Transform.rotateY(.002 * (Date.now() - initialTime));
+    }
+});
+
+mainContext.add(centerSpinModifier).add(logo);
